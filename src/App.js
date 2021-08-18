2531696d11f435ab12f9392a152ab140ae1453e5
chriscrm <cristian.monterrosa@gmail.com>
2021-08-18 13:56:04 -0600

    adding CardList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Card from "./Components/Card";
+import CardList from "./Components/CardList";
 import { robots } from "./robots";
 
 function App() {
   return (
     <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} />
-      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+      <CardList robots={robots} />
     </div>
   );
 }
